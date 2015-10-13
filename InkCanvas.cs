fa9ff1135c20b8c04506712afd01766691178723
Terrance Mok <tthmok@gmail.com>
2015-10-13 13:42:28 -0600

    Add David's Vector and InkCanvas classes.

---
+++ InkCanvas.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace WPUnlock
+{
+    public class ColorSet
+    {
+        public static SolidColorBrush Black = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 51, 51, 51));
+        public static SolidColorBrush White = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 255, 255));
+
+        public static SolidColorBrush Red = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 220, 20, 60));
+        public static SolidColorBrush Blue = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 0, 0, 205));
+        public static SolidColorBrush Yellow = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 215, 0));
+
+        public static SolidColorBrush Orange = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 237, 126, 34));
+        public static SolidColorBrush Green = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 59, 163, 64));
+        public static SolidColorBrush Purple = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 86, 51, 147));
+
+    }
+    public class InkCanvas : Canvas
+    {
+        #region Instance Variables
+
+        public double strokeWeight = 5;
+        public SolidColorBrush stroke;
+        private Dictionary<uint, Path> FingerPaths = new Dictionary<uint, Path>();
+        private bool isDrawingEnabled = true;
+        private List<Path> redoPaths = new List<Path>();
+
+        #endregion
+
+        #region Properties
+
+        public SolidColorBrush Stroke
+        {
+            get
+            {
+                return this.stroke;
+            }
+            set
+            {
+                this.stroke = value;
+            }
+        }
+
+        public double StrokeWeight
+        {
+            get
+            {
+                return this.strokeWeight;
+            }
+            set
+            {
+                this.strokeWeight = value;
+            }
+        }
+
+        public bool IsDrawingEnabled
+        {
+            get
+            {
+                return this.isDrawingEnabled;
+            }
+            set
+            {
+                this.isDrawingEnabled = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public InkCanvas()
+        {
+            this.PointerPressed += OnPointerPressed;
+            this.PointerMoved += OnPointerMoved;
+            this.PointerReleased += OnPointerReleased;
+
+            this.Background = ColorSet.White;
+            this.stroke = ColorSet.Black;
+        }
+
+        #endregion
+
+        #region Touch Events
+
+        void OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (this.isDrawingEnabled)
+                CreatePath(e.Pointer.PointerId, new Point(e.GetCurrentPoint(this).Position.X, e.GetCurrentPoint(this).Position.Y));
+        }
+
+        void OnPointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (this.isDrawingEnabled)
+                UpdatePath(e.Pointer.PointerId, new Point(e.GetCurrentPoint(this).Position.X, e.GetCurrentPoint(this).Position.Y));
+        }
+
+        void OnPointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (this.isDrawingEnabled)
+            {
+                UpdatePath(e.Pointer.PointerId, new Point(e.GetCurrentPoint(this).Position.X, e.GetCurrentPoint(this).Position.Y));
+                CheckIfEmptyPath(e.Pointer.PointerId);
+                this.FingerPaths.Remove(e.Pointer.PointerId);
+
+            }
+        }
+
+        #endregion
+
+        #region Drawing Methods
+
+        private void CreatePath(uint id, Point position)
+        {
+            Path pathForCurrentFinger = new Path();
+            this.FingerPaths.Add(id, pathForCurrentFinger);
+            this.Children.Add(pathForCurrentFinger);
+
+            pathForCurrentFinger.StrokeThickness = this.strokeWeight;
+            pathForCurrentFinger.Stroke = this.stroke;
+
+            PathGeometry geometryForCurrentFinger = new PathGeometry();
+            geometryForCurrentFinger.Figures = new PathFigureCollection();
+
+            PathFigure f = new PathFigure();
+            f.StartPoint = new Point(position.X, position.Y);
+            geometryForCurrentFinger.Figures.Add(f);
+
+            pathForCurrentFinger.Data = geometryForCurrentFinger;
+            System.Diagnostics.Debug.WriteLine("DEBUG! - PATH CREATED");
+            this.redoPaths.Clear();
+        }
+
+        private void UpdatePath(uint id, Point position)
+        {
+            PathGeometry geometryForCurrentFinger = this.FingerPaths[id].Data as PathGeometry;
+            QuadraticBezierSegment segment = new QuadraticBezierSegment();
+
+            if (geometryForCurrentFinger.Figures[0].Segments.Count > 0)
+            {
+                QuadraticBezierSegment lastLine = geometryForCurrentFinger.Figures[0].Segments.Last() as QuadraticBezierSegment;
+                Point lastEndPoint = lastLine.Point2;
+                segment.Point1 = lastEndPoint;
+            }
+            else
+            {
+                segment.Point1 = geometryForCurrentFinger.Figures[0].StartPoint;
+                segment.Point2 = position;
+            }
+
+            segment.Point2 = position;
+            geometryForCurrentFinger.Figures[0].Segments.Add(segment);
+            geometryForCurrentFinger.Figures[0].IsFilled = true;
+        }
+
+        private void CheckIfEmptyPath(uint id)
+        {
+            double length = 0;
+            PathGeometry geometryForCurrentFinger = this.FingerPaths[id].Data as PathGeometry;
+            if (geometryForCurrentFinger.Figures[0].Segments.Count > 0)
+            {
+                foreach (QuadraticBezierSegment segment in geometryForCurrentFinger.Figures[0].Segments)
+                {
+                    length += Utils.Dist(segment.Point1, segment.Point2);
+                }
+            }
+            if (length == 0)
+            {
+                this.Children.Remove(this.FingerPaths[id]);
+            }
+
+        }
+
+        public void ClearStrokes()
+        {
+            this.Children.Clear();
+        }
+
+        public void Undo()
+        {
+            if (this.Children.Count > 0)
+            {
+                this.redoPaths.Add(this.Children.Last() as Path);
+
+                System.Diagnostics.Debug.WriteLine("Undid " + this.Children.Last() + " :: " + this.Children.Count);
+                this.Children.Remove(this.Children.Last());
+            }
+        }
+
+        public void Redo()
+        {
+            if (redoPaths.Count != 0)
+            {
+                this.Children.Add(this.redoPaths.First());
+                this.redoPaths.Remove(this.redoPaths.First());
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Input;
+
+namespace WPUnlock
+{
+    /// <summary>
+    /// From David Ledo: http://cpsc581.davidledo.com/tutorials/code/Vector.cs
+    /// Code partially based on http://www.codeproject.com/Articles/17425/A-Vector-Type-for-C
+    /// </summary>
+    public class Vector
+    {
+        public static readonly Vector Origin = new Vector(0, 0);
+        public static readonly Vector X_Axis = new Vector(1, 0);
+        public static readonly Vector Y_Axis = new Vector(0, 1);
+
+        #region Instance Variables
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        #endregion
+
+        #region Properties
+
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(LengthSquared);
+            }
+        }
+
+        public double LengthSquared
+        {
+            get
+            {
+                return SumComponentSqrs(this);
+            }
+        }
+
+        #endregion
+
+        #region Operator Overload
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector
+            (
+                v1.X + v2.X,
+                v1.Y + v2.Y
+            );
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector
+            (
+                v1.X - v2.X,
+                v1.Y - v2.Y
+            );
+
+        }
+
+        public static Vector operator -(Vector v1)
+        {
+            return new Vector
+            (
+                -v1.X,
+                -v1.Y
+            );
+        }
+
+        public static bool operator <(Vector v1, Vector v2)
+        {
+            return v1.Length < v2.Length;
+        }
+
+        public static bool operator <=(Vector v1, Vector v2)
+        {
+            return v1.Length <= v2.Length;
+        }
+
+        public static bool operator >(Vector v1, Vector v2)
+        {
+            return v1.Length > v2.Length;
+        }
+
+        public static bool operator >=(Vector v1, Vector v2)
+        {
+            return v1.Length >= v2.Length;
+        }
+
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            return
+            (
+                v1.X == v2.X &&
+                v1.Y == v2.Y
+            );
+        }
+
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public static Vector operator /(Vector v1, double s2)
+        {
+            return new Vector
+            (
+                v1.X / s2,
+                v1.Y / s2
+            );
+        }
+
+        public static Vector operator *(Vector v1, double s2)
+        {
+            return new Vector
+            (
+                v1.X * s2,
+                v1.Y * s2
+            );
+        }
+
+        public static Vector operator *(double s1, Vector v2)
+        {
+            return v2 * s1;
+        }
+
+        #endregion
+
+        #region Operations
+
+        public static double CrossProduct(Vector v1, Vector v2)
+        {
+            return v1.X * v2.Y - v1.Y * v2.X;
+        }
+
+        public double CrossProduct(Vector other)
+        {
+            return CrossProduct(this, other);
+        }
+
+        public static double DotProduct(Vector v1, Vector v2)
+        {
+            return v1.X * v2.X + v1.Y + v2.Y;
+        }
+
+        public double DotProduct(Vector other)
+        {
+            return DotProduct(this, other);
+        }
+
+        public static bool IsUnitVector(Vector v1)
+        {
+            return v1.Length == 1;
+        }
+
+        public bool IsUnitVector()
+        {
+            return IsUnitVector(this);
+        }
+
+        public static Vector Normalize(Vector v1)
+        {
+            // Check for divide by zero errors
+            if (v1.Length == 0)
+            {
+                throw new DivideByZeroException("Magnitude for the Vector is currently 0");
+            }
+            else
+            {
+                // find the inverse of the vectors magnitude
+                double inverse = 1 / v1.Length;
+                return
+                (
+                   new Vector
+                   (
+                    // multiply each component by the inverse of the magnitude
+                      v1.X * inverse,
+                      v1.Y * inverse
+                   )
+                );
+            }
+        }
+
+        public static Vector Interpolate(Vector v1, Vector v2, double control)
+        {
+            if (control > 1 || control < 0)
+            {
+                // Error message includes information about the actual value of the 
+                // argument
+                throw new ArgumentOutOfRangeException
+                (
+                   "Control Point must be between 0 and 1"
+                );
+            }
+            else
+            {
+                return
+                (
+                   new Vector
+                   (
+                       v1.X * (1 - control) + v2.X * control,
+                       v1.Y * (1 - control) + v2.Y * control
+                    )
+                );
+            }
+        }
+
+        public Vector Interpolate(Vector other, double control)
+        {
+            return Interpolate(this, other, control);
+        }
+
+        public static Double Abs(Vector v1)
+        {
+            return v1.Length;
+        }
+
+        public static double Angle(Vector v1, Vector v2)
+        {
+            Vector a = Normalize(v1);
+            Vector b = Normalize(v2);
+            double angle = (Math.Atan2(a.Y, a.X) - Math.Atan2(b.Y, b.X));
+            if (angle < 0)
+            {
+                angle += Math.PI * 2;
+            }
+            return
+             (
+                Math.Acos
+                (
+                   Normalize(v1).DotProduct(Normalize(v2))
+                )
+             );
+        }
+
+        public static double AnglePI(Vector v1, Vector v2)
+        {
+            double angle = Angle2PI(v1, v2);
+            if (angle > Math.PI)
+            {
+                angle = (2 * Math.PI) - angle;
+            }
+            return angle;
+        }
+
+        public static double Angle2PI(Vector v1, Vector v2)
+        {
+            Vector a = Normalize(v1);
+            Vector b = Normalize(v2);
+            double angle = (Math.Atan2(a.Y, a.X) - Math.Atan2(b.Y, b.X));
+            if (angle < 0)
+            {
+                angle += Math.PI * 2;
+            }
+            return angle;
+        }
+
+        public double Angle(Vector other)
+        {
+            return Angle(this, other);
+        }
+
+        public static Vector Max(Vector v1, Vector v2)
+        {
+            if (v1 >= v2) { return v1; }
+            return v2;
+        }
+
+        public Vector Max(Vector other)
+        {
+            return Max(this, other);
+        }
+
+        public static Vector Min(Vector v1, Vector v2)
+        {
+            if (v1 <= v2) { return v1; }
+            return v2;
+        }
+
+        public Vector Min(Vector other)
+        {
+            return Min(this, other);
+        }
+
+
+        public static bool IsPerpendicular(Vector v1, Vector v2)
+        {
+            return v1.DotProduct(v2) == 0;
+        }
+
+        public bool IsPerpendicular(Vector other)
+        {
+            return IsPerpendicular(this, other);
+        }
+
+        public static double SumComponents(Vector v1)
+        {
+            return (v1.X + v1.Y);
+        }
+
+        public double SumComponents()
+        {
+            return SumComponents(this);
+        }
+
+        public static Vector PowComponents(Vector v1, double power)
+        {
+            return
+            (
+               new Vector
+               (
+                  Math.Pow(v1.X, power),
+                  Math.Pow(v1.Y, power)
+               )
+            );
+        }
+
+        public static Vector SqrtComponents(Vector v1)
+        {
+            return
+            (
+               new Vector
+               (
+                  Math.Sqrt(v1.X),
+                  Math.Sqrt(v1.Y)
+               )
+            );
+        }
+
+        public static Vector SqrComponents(Vector v1)
+        {
+            return
+            (
+               new Vector
+               (
+                   v1.X * v1.X,
+                   v1.Y * v1.Y
+               )
+             );
+        }
+
+
+        public static double SumComponentSqrs(Vector v1)
+        {
+            Vector v2 = SqrComponents(v1);
+            return v2.SumComponents();
+        }
+
+        public override int GetHashCode()
+        {
+            return
+            (
+               (int)((X + Y) % Int32.MaxValue)
+            );
+        }
+
+        public override string ToString()
+        {
+            return "Vector:: X = " + this.X.ToString("0.000") + ", " + this.Y.ToString("0.000");
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Vector()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+
+        public Vector(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        #endregion
+
+
+    }
+}
